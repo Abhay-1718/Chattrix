b842691f3f84f301e804a1165752487a05bc0c23
Abhay-1718 <abhaysingh80766@gmail.com>
2024-11-25 21:50:23 +0530

    cors error solved

--- frontend/src/lib/api-client.js
+++ frontend/src/lib/api-client.js
@@ -3,6 +3,7 @@ import { HOST } from "../utils/constant.js";
 
 const apiClient = axios.create({
     baseURL: HOST,
+    withCredentials: true, // Include credentials for cookie-based auth
 });
 
-export default apiClient; 
+export default apiClient;
